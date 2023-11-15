51d7ef79d7124141a9abdd88d3e2ac940f4b987f
DAchoMI <tyrnsorl123@naver.com>
2023-11-15 17:16:41 +0900

    DungeonRoomNodeEditor

---
+++ DungeonGunner/Assets/Scripts/GameManager/GameResources.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameResources : MonoBehaviour
+{
+    public static GameResources _instance;
+    public static GameResources Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                _instance = Resources.Load<GameResources>("GameResources");
+            }
+
+            return _instance;
+        }
+    }
+
+    [Space(10)]
+    [Header("Dungeon")]
+    public RoomNodeTypeListSO _roomNodeTypeList;
+}
