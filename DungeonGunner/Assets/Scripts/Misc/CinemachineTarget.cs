49e09f6679bce55216318831f73d322e0e371f30
DAchoMI <tyrnsorl123@naver.com>
2023-11-28 17:47:51 +0900

    Cinemachine Target Group
    - For During Aim

--- DungeonGunner/Assets/Scripts/Misc/CinemachineTarget.cs
+++ DungeonGunner/Assets/Scripts/Misc/CinemachineTarget.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 [RequireComponent(typeof(CinemachineTargetGroup))]
 public class CinemachineTarget : MonoBehaviour
 {
+    [SerializeField] Transform _cursorTarget;
+
     CinemachineTargetGroup _cinemachineTargetGroup;
 
     private void Awake()
@@ -27,8 +29,20 @@ public class CinemachineTarget : MonoBehaviour
             target = GameManager.Instance.GetPlayer().transform
         };
 
-        CinemachineTargetGroup.Target[] cinemachineTargetArray = new CinemachineTargetGroup.Target[] { cinemachineTarget_Player };
+        CinemachineTargetGroup.Target cinemachineTarget_Cursor = new CinemachineTargetGroup.Target
+        {
+            weight = 1f,
+            radius = 5f,
+            target = _cursorTarget
+        };
+
+        CinemachineTargetGroup.Target[] cinemachineTargetArray = new CinemachineTargetGroup.Target[] { cinemachineTarget_Player, cinemachineTarget_Cursor };
 
         _cinemachineTargetGroup.m_Targets = cinemachineTargetArray;
+    }
+
+    private void Update()
+    {
+        _cursorTarget.position = HelperUtilities.GetMouseWorldPosition();
     }
 }
